01de971cedd83e13c35191cccfba94c9398f35b0
star <chlvkfksquf@gmail.com>
2023-02-09 18:06:49 +0900

    Add fetchRepoIssues

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,5 @@
 import { Octokit } from "octokit";
+import Axios from "axios";
 
 const octokit = new Octokit({
   auth: process.env.REACT_APP_GIT_TOKEN,
@@ -9,6 +10,14 @@ export const fetchRepos = async (username: string) => {
     username: username,
     sort: "updated",
   });
+
+  return res;
+};
+
+export const fetchRepoIssues = async (username: string, repository: string) => {
+  const res = Axios.get(
+    `https://api.github.com/repos/${username}/${repository}/issues`
+  );
 
   return res;
 };
