602534d2239664ddfb3293d43627dfef4e2338ad
star <chlvkfksquf@gmail.com>
2023-02-09 11:40:47 +0900

    Add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 
 import Main from "pages/Main";
+import Navbar from "components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-      </Routes>
+      <RecoilRoot>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </RecoilRoot>
     </BrowserRouter>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { useSetRecoilState } from "recoil";
+
+import { fetchRepos } from "api";
+import { reposState } from "store";
+import { Repo } from "type";
+
+function Navbar() {
+  const [search, setSearch] = useState<string>("");
+  const setRepos = useSetRecoilState<Repo[]>(reposState);
+
+  const onChangeSearch = (e: any) => {
+    e.preventDefault();
+    setSearch(e.target.value);
+  };
+
+  const onSearch = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(typeof search);
+    if (typeof search === "undefined" || search === "") {
+      setRepos([]);
+    } else {
+      const res = await fetchRepos(search);
+      console.log(123, res);
+      setRepos(res.data);
+    }
+  };
+
+  return (
+    <div className="inline-flex items-center justify-between w-full px-8 bg-[#161B22] h-14">
+      <div className="text-white">로고</div>
+      <form onSubmit={(e) => onSearch(e)}>
+        <input
+          placeholder="Find a repository"
+          type="text"
+          value={search}
+          onChange={onChangeSearch}
+          className="w-full rounded-md border border-[#e0e0e0] bg-white py-2 px-3 text-[#6B7280] outline-none focus:border-red-500 focus:shadow-md"
+        />
+      </form>
+    </div>
+  );
+}
+
+export default Navbar;
