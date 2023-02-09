0caabf6b0a6d8c732d1e2a60ccf974799fb9d8e7
star <chlvkfksquf@gmail.com>
2023-02-09 12:32:25 +0900

    Add subscriberepos

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,17 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
 
 import { Repo } from "type";
+
+const { persistAtom } = recoilPersist();
 
 export const reposState = atom<Repo[]>({
   key: "repos",
   default: [],
+});
+
+export const reposSubscribeState = atom<Repo[]>({
+  key: "reposSubscribe",
+  default: [],
+  effects_UNSTABLE: [persistAtom],
 });
