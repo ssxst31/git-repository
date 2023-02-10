acba370e85974f036d3d20cd446890d2660802da
star <chlvkfksquf@gmail.com>
2023-02-11 01:15:16 +0900

    Add arrowUp button

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -7,11 +7,21 @@ import RepoCard from "components/RepoCard";
 function Main() {
   const reposSubscribe = useRecoilValue<Repo[]>(reposSubscribeState);
 
+  const moveToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <div className="pt-16 mx-auto space-y-2 w-96">
       {reposSubscribe.map((repo) => (
         <RepoCard key={repo.id} repo={repo} showIssue={!!repo.open_issues} />
       ))}
+      <div
+        className="fixed cursor-pointer bottom-4 right-4"
+        onClick={moveToTop}
+      >
+        <img className="w-14 h-14" src="/icons/arrow-up.png" alt="arrow-up" />
+      </div>
     </div>
   );
 }
